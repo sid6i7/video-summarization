115820fc9fec9e25d1dbe2b25fa7d9448ca541ea
Tanish <tanishkushalkar@gmail.com>
2022-06-29 17:35:04 +0530

    add statics

--- providers/scraper.py
+++ providers/scraper.py
@@ -3,16 +3,23 @@ import json
 import requests as rq
 import sys
 import os
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from config import *
 
 arg = '+'.join(sys.argv[1:])
-ini = rq.get(f'https://www.youtube.com/results?search_query={arg}&sp=EgIYAQ%253D%253D').text
+ini = rq.get(f'{YT_SEARCH_URL}{arg}{YT_SEARCH_FILTER}').text
 dyn = re.search('var ytInitialData =(.*?);</script>', ini)[0][20:-10]
 js = json.loads(dyn)
-part = js['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents']
+videos = js['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents']
 
 print('Downloading...')
 try:
-    for x in part:
-        print(x['videoRenderer']['videoId'])
-        os.system(f'youtube-dl -x --audio-format wav https://www.youtube.com/watch?v={x["videoRenderer"]["videoId"]} -o yt-aud/{x["videoRenderer"]["videoId"]}.wav')
+    for video in videos:
+        print(video['videoRenderer']['videoId'])
+        os.system(f'youtube-dl -x --audio-format wav https://www.youtube.com/watch?v={video["videoRenderer"]["videoId"]} -o {AUDIO_DIR}/{video["videoRenderer"]["videoId"]}.wav')
 except: None
